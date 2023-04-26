ae40bd00d2a881726f24163b1aa3555e5a48200d
andresdavidleonolivero <131920275+andresdavidleonolivero@users.noreply.github.com>
2023-04-26 16:36:34 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.py
@@ -0,0 +1,3 @@
+mbit_Robot.car_ctrl_speed(mbit_Robot.CarState.CAR_RUN, 255)
+basic.pause(2000)
+mbit_Robot.car_ctrl_speed(mbit_Robot.CarState.CAR_RUN, 0)
---
+++ main.ts
@@ -0,0 +1,3 @@
+mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Run, 255)
+basic.pause(2000)
+mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Run, 0)
